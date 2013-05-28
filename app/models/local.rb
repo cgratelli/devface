d746ae74613037851ff1f257b28ca91b979e399c
manuel <mfrancia_h@hotmail.com>
2013-05-28 01:02:55 -0500

    add local with google maps

--- app/models/local.rb
+++ app/models/local.rb
@@ -1,3 +1,3 @@
 class Local < ActiveRecord::Base
-  attr_accessible :address, :address_google_maps, :distrit, :first_phone, :name
+  attr_accessible :address, :distrit, :first_phone, :name, :lat, :lang
 end
